ee180b519b3dbb17898b4f8580d5c76fc4a02e29
Pong Cheecharern <k.cheecharern@gmail.com>
2017-03-23 06:59:14 +0000

    added paperclip and image in show file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323004432) do
+ActiveRecord::Schema.define(version: 20170323063548) do
 
   create_table "apps", force: :cascade do |t|
     t.string   "title"
@@ -30,9 +30,13 @@ ActiveRecord::Schema.define(version: 20170323004432) do
     t.string   "title"
     t.text     "description"
     t.string   "link"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "category_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
