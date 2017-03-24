ea4bf2da8780ea7ec0452dcb80d23062b0e20d4e
Pong Cheecharern <k.cheecharern@gmail.com>
2017-03-24 04:37:18 +0000

    add bootstrap panels to app

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,5 +1,5 @@
 class Tool < ActiveRecord::Base
     belongs_to :category
-     has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+     has_attached_file :image, styles: { medium: "300x300>", thumb: "200x200>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
